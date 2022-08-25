b52863ca3dea75145b187009aafa86d7b8d98ea7
blockgame <1221024707@njupt.edu.cn>
2022-08-25 10:04:16 +0800

    update webdriver config

--- tweet-bot.py
+++ tweet-bot.py
@@ -2,29 +2,25 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.chrome.options import Options
-import time,random,os
+import time,random,os,datetime
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-work_dir = os.path.dirname(os.path.abspath(__file__)) + "/session"
-if not os.path.exists(work_dir):
-    os.makedirs(work_dir)
-
 class TwitterBot:
 
     def __init__(self, username, password):
+        work_dir = os.path.dirname(os.path.abspath(__file__)) + "/" + username
+        if not os.path.exists(work_dir):
+            os.makedirs(work_dir)
         self.username = username
         self.password = password
         options=Options()
-        #options.add_argument('incognito')  # 隐身模式（无痕模式）
-        options.add_experimental_option("excludeSwitches", ["enable-automation"])
-        options.add_experimental_option('useAutomationExtension', False)
-        options.add_argument('disable-extensions')
+        options.add_argument('--headless')
+        options.add_argument('--no-sandbox') 
+        options.add_argument("--window-size=1400,600")
         options.add_argument('--user-data-dir=' + work_dir)
-        options.add_argument("profile-directory=" + username)
         self.bot = webdriver.Chrome(options=options)
-        self.bot.maximize_window()
 
     def login(self):
         bot = self.bot
@@ -36,22 +32,20 @@ class TwitterBot:
         else:
             username = bot.find_element_by_name("text")
             username.clear()
-            #password.clear()
             username.send_keys(self.username)
-            time.sleep(2)
+            time.sleep(1)
             element_login = bot.find_element_by_xpath('//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[6]/div')
             element_login.click()
-            time.sleep(2) 
+            time.sleep(1) 
             element_pass = bot.find_element_by_name("password")
             element_pass.clear()
             element_pass.send_keys(password)
-            time.sleep(2) 
+            time.sleep(1) 
             element_login = bot.find_element_by_xpath('//*[@data-testid="LoginForm_Login_Button"]')
             element_login.click()
             time.sleep(2) 
             loginError = bot.find_elements_by_xpath(
                 "//span[contains(text(), 'match our records')]")  # not the best solution, sometimes works with few words
-            time.sleep(5)
             if not loginError:
                 print("Login Successful!")
                 return True
@@ -60,7 +54,7 @@ class TwitterBot:
                 bot.close()
                 return False
             print(loginError)
-            time.sleep(5)
+            time.sleep(2)
 
     def send_tweet(self,text):
         bot = self.bot
@@ -69,6 +63,8 @@ class TwitterBot:
         element_text.send_keys(text)
         tweet_button = bot.find_element_by_xpath('//*[@data-testid="tweetButtonInline"]')
         bot.execute_script("arguments[0].click();", tweet_button)
+        print("send thread success")
+        time.sleep(2)
 
     def find_action(self, term):
         links = list()
@@ -114,38 +110,49 @@ class TwitterBot:
             like_button = WebDriverWait(bot, 3).until(EC.presence_of_element_located((By.XPATH, "//div[@aria-label='Like']")))
             like_button.click()
         except:
-            pass
-        try:
+            print("liked")
+        try: 
             retweet_button = WebDriverWait(bot, 3).until(EC.presence_of_element_located((By.XPATH, "//div[@aria-label='Retweet']")))
             retweet_button.click()
             retweet_confirm = WebDriverWait(bot, 3).until(EC.presence_of_element_located((By.XPATH, "//div[@data-testid='retweetConfirm']")))
             retweet_confirm.click()
         except:
-            pass
+            print("retweeted")
         comment_text = WebDriverWait(bot, 3).until(EC.presence_of_element_located((By.XPATH, "//div[@data-testid='tweetTextarea_0']")))
         comment_text.send_keys(sq)
         time.sleep(2)
-        replybutton=bot.find_element_by_css_selector('div[role="button"][data-testid="tweetButtonInline"]').click()
+        bot.find_element_by_css_selector('div[role="button"][data-testid="tweetButtonInline"]').click()
         print("一键三连成功")
 
     def follow_user(self,url):
         bot = self.bot
         #bot.get(url)
+        #time.sleep(3)
         page_cards = bot.find_elements_by_xpath('//div[@data-testid="UserCell"]')
         for card in page_cards:
             try:
-                followbutton = WebDriverWait(bot, 3).until(EC.element_to_be_clickable((By.XPATH, '//span[text()="Follow"]')))
+                followbutton = WebDriverWait(bot, 5).until(EC.element_to_be_clickable((By.XPATH, '//span[text()="Follow"]')))
                 followbutton.click()
                 print("follow success")
                 time.sleep(2)
-            except:
+            except Exception as e:
                 print("already following")
 
-username = "user"
-password = "pass"
-sq=' good luck!'
-term = "giveaway WL follow"
-bix = TwitterBot(username, password)
-loginSuccess = bix.login()
-if loginSuccess:
-    bix.find_action(term)
+    def __enter__(self):
+        return self
+
+    def __exit__(self, type, value, trace):
+        print("bot work finish")
+        self.bot.close()
+        self.bot.quit()
+
+username = "username"
+password = "passwd"
+sq='good luck!'
+term='whitelist giveaway'
+with TwitterBot(username, password) as bix:
+    loginSuccess = bix.login()
+    if loginSuccess:
+        #bix.send_tweet("this is at arm os post")
+        bix.find_action(term)
+        
